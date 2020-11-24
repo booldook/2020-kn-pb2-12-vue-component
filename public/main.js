98ac2b39980a8a2dd1546233042f1f4745dcd1b6
booldook <booldook@gmail.com>
2020-11-24 14:44:14 +0900

    export, import

--- public/main.js
+++ public/main.js
@@ -1,10 +1,10 @@
-import calc from './sample.js';
+// import calc from './sample.js';
 import { sum, multi } from './sample.js';
-import sum2 from './sample2.js';
+import sumObj from './sample2.js';
 
 // console.log( calc.sum(10, 20) );
 // console.log( calc.multi(10, 20) );
 
 console.log( sum(10, 20) );
 console.log( multi(10, 20) );
-console.log( sum2(10, 20) );
+console.log( sumObj.sum() );
--- public/sample2.js
+++ public/sample2.js
@@ -1,3 +1,9 @@
-export default sum2 = (a, b) => {
-	return a + b;
+// export default "A";
+// export default (a, b) => a + b;
+export default {
+	a: 1000,
+	b: 2000,
+	sum() {
+		return this.a + this.b;
+	}
 }
