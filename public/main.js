a9e4a25a9c34c1c44cec39dc1984ce6e84988cc8
booldook <booldook@gmail.com>
2020-11-24 13:06:23 +0900

    module system

---
+++ public/main.js
@@ -0,0 +1,4 @@
+import { sum, multi } from './sample.js';
+
+console.log( sum(10, 20) );
+console.log( multi(10, 20) );
---
+++ public/sample.js
@@ -0,0 +1,9 @@
+const sum = (a, b) => {
+	return a + b;
+}
+
+const multi = (a, b) => {
+	return a * b;
+}
+
+export { sum, multi }
